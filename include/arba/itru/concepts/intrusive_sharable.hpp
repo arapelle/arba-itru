4a366161a51fdffbaa99dfcf4046bf3c657f3c21
Aymeric Pellé <aympelle@gmail.com>
2023-09-19 11:15:53 +0200

    v0.2.0: Move and split the concepts intrusive_sharable and intrusive_latent.

---
+++ include/arba/itru/concepts/intrusive_sharable.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+inline namespace arba
+{
+namespace itru::concepts
+{
+
+template <class element_type>
+void intrusive_shared_ptr_add_ref(element_type* ptr) noexcept;
+
+template <class element_type>
+void intrusive_shared_ptr_release(element_type* ptr) noexcept;
+
+template <class element_type>
+concept intrusive_sharable = requires(element_type* ptr)
+{
+    intrusive_shared_ptr_add_ref(ptr);
+    intrusive_shared_ptr_release(ptr);
+};
+
+}
+}
