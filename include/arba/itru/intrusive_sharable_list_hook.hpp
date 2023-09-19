51de8a7b527669693143e37c820646aa0c4a3fab
Aymeric Pellé <aympelle@gmail.com>
2023-09-19 11:19:40 +0200

    v0.2.0:#2: Add class intrusive_sharable_list_hook<T>, a helper class to create intrusive list node types.

---
+++ include/arba/itru/intrusive_sharable_list_hook.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "intrusive_shared_ptr.hpp"
+
+inline namespace arba
+{
+namespace itru
+{
+
+template <typename IntrusiveListNodeT>
+class intrusive_sharable_list_hook
+{
+public:
+    inline IntrusiveListNodeT* previous() const noexcept { return previous_; }
+    inline IntrusiveListNodeT*& previous() noexcept { return previous_; }
+    inline const intrusive_shared_ptr<IntrusiveListNodeT>& next() const noexcept { return next_; }
+    inline intrusive_shared_ptr<IntrusiveListNodeT>& next() noexcept { return next_; }
+    void unhook()
+    {
+        next_->previous() = previous_;
+        IntrusiveListNodeT* previous = previous_;
+        previous_ = nullptr;
+        previous->next_ = std::move(std::move(next_));
+    }
+
+private:
+    IntrusiveListNodeT* previous_ = nullptr;
+    intrusive_shared_ptr<IntrusiveListNodeT> next_;
+};
+
+}
+}
