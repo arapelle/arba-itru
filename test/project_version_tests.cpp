4b4364182656850247f3f99f6fe01f2e68d56b41
Aymeric Pellé <aympelle@gmail.com>
2025-01-28 15:30:49 +0100

    v0.3.0: Start dev version.

--- test/project_version_tests.cpp
+++ test/project_version_tests.cpp
@@ -5,7 +5,7 @@
 TEST(project_version_tests, test_version_macros)
 {
     const unsigned major = 0;
-    const unsigned minor = 2;
+    const unsigned minor = 3;
     const unsigned patch = 0;
     ASSERT_EQ(ARBA_ITRU_VERSION_MAJOR, major);
     ASSERT_EQ(ARBA_ITRU_VERSION_MINOR, minor);
